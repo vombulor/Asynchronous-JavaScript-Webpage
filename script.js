4eb45f0b59b35d0a42b927863f17892266a5b50b
LAPTOP-TROE5J8J\thedi <thedirtyponcho@gmail.com>
2026-02-15 22:28:57 -0600

    added dropdown population using REST Countries API

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 const countrySelect = document.getElementById("countrySelect");
 const countryInfo = document.getElementById("countryInfo");
 
-const allCountriesURL = "https://restcountries.com/v3.1/all";
+const allCountriesURL = "https://restcountries.com/v3.1/all?fields=name";
 const countryByNameURL = "https://restcountries.com/v3.1/name/";
 
 const loadCountries = async () => {
+    try {
+        const response = await fetch(allCountriesURL);
+
+        if (!response.ok) {
+            throw new Error("Failed to fetch countries");
+        }
+
+        const data = await response.json();
+
+        data.sort((a, b) =>
+            a.name.common.localeCompare(b.name.common)
+        );
+
+        data.forEach(country => {
+            const option = document.createElement("option");
+            option.value = country.name.common;
+            option.textContent = country.name.common;
+            countrySelect.appendChild(option);
+        });
+
+    } catch (error) {
+        countryInfo.innerHTML = "<p>Error loading countries.</p>";
+    }
 };
+
 
 const getCountryDetails = async (countryName) => {
 };
